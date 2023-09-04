4db16cd83f93012aebd583fef966db2940337687
WAF007B <129391009+WAF007B@users.noreply.github.com>
2023-09-04 19:02:31 +0200

    github gists and a new msg board with github api

---
+++ script.js
@@ -0,0 +1,70 @@
+// Function to send a message to GitHub Gists
+async function sendMessage() {
+    const messageText = document.getElementById('message').value;
+    if (messageText.trim() !== '') {
+        try {
+            // Create a new message object
+            const message = { text: messageText };
+
+            // Fetch the existing Gist content
+            const gistId = '9fec2f7e61d309d91cb7f735e9f53556'; // Replace with your Gist ID
+            const response = await fetch(`https://api.github.com/gists/${gistId}`);
+            const data = await response.json();
+
+            // Update the Gist content with the new message
+            data.files['message-board.json'].content = JSON.stringify([...JSON.parse(data.files['message-board.json'].content), message]);
+
+            // Send a PATCH request to update the Gist
+            const updateResponse = await fetch(`https://api.github.com/gists/${gistId}`, {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    files: {
+                        'message-board.json': {
+                            content: data.files['message-board.json'].content,
+                        },
+                    },
+                }),
+                headers: {
+                    'Authorization': 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, // Replace with your GitHub Personal Access Token
+                },
+            });
+
+            if (updateResponse.status === 200) {
+                // Clear the input field
+                document.getElementById('message').value = '';
+                // Reload messages
+                displayMessages();
+            } else {
+                console.error('Failed to update Gist:', updateResponse.statusText);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+}
+
+// Function to display messages (fetch from GitHub Gists)
+async function displayMessages() {
+    try {
+        // Fetch the Gist content
+        const gistId = 'YOUR_GIST_ID'; // Replace with your Gist ID
+        const response = await fetch(`https://api.github.com/gists/${gistId}`);
+        const data = await response.json();
+
+        // Parse the content and display messages
+        const messages = JSON.parse(data.files['message-board.json'].content);
+        const messageList = document.getElementById('messageList');
+        messageList.innerHTML = '';
+
+        messages.forEach((message) => {
+            const li = document.createElement('li');
+            li.innerText = message.text;
+            messageList.appendChild(li);
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Call displayMessages() to load and display existing messages when the page loads.
+displayMessages();
